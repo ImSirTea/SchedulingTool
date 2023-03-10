5f63b45eace020eed4c4577c46c3a3322d010d60
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-10 17:47:53 +0000

    Barebones init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import VsScheduler from './components/VsScheduler.vue';
+
+</script>
+
+<template>
+  <div class="container">
+    <vs-scheduler />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 75vw;
+  height: 75vh;
+}
+</style>
---
+++ src/components/VsScheduler.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+export interface VsSchedulerEvent {
+  startDate: Date;
+  plannedEndDate: Date;
+}
+
+export interface VsSchedulerEventGroup {
+  name: string;
+  events: VsSchedulerEvent[];
+}
+
+export interface VsSchedulerProps {
+  events: VsSchedulerEventGroup[];
+}
+
+const props = defineProps<VsSchedulerProps>();
+
+</script>
+
+<template>
+  <div class="vs-scheduler">
+    <slot name="groups" />
+  </div>
+</template>
+
+<style scoped>
+.vs-scheduler {
+  height: 100%;
+  width: 100%;
+  outline: 1px solid black;
+}
+</style>
