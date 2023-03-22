dd84df07ce02141db916533a6754c68d0318ab80
Adam <adam@premiersystems.com>
2023-03-22 11:43:20 +0000

    Move to event control

--- src/components/subcomponents/VsSchedulerEvent.vue
+++ src/components/subcomponents/VsSchedulerEvent.vue
@@ -1,7 +1,50 @@
 <script lang="ts" setup>
+import type { VsSchedulerEvent } from '../VsScheduler.vue'
+import { ChronoUnit, LocalDate } from '@js-joda/core'
+
+export interface VsSchedulerEventProps {
+  event: VsSchedulerEvent
+  cellWidth: number
+  earliestStartDate: LocalDate
+}
+
+const props = defineProps<VsSchedulerEventProps>()
+
+const SPACING_OFFSET = 2
+const HEIGHT_OFFSET = 8
+
+function numberOfDaysInEvent(event: VsSchedulerEvent): number {
+  return ChronoUnit.DAYS.between(event.startDate, event.endDate)
+}
+
+function eventWidth(event: VsSchedulerEvent): number {
+  return props.cellWidth * numberOfDaysInEvent(event) - SPACING_OFFSET - 1 // @TODO: Could use a CSS var to control spacing, -1 for border
+}
+
+function daysBetweenOriginAndEventStart(event: VsSchedulerEvent): number {
+  return ChronoUnit.DAYS.between(props.earliestStartDate, event.startDate)
+}
+
+function eventOffsetLeft(event: VsSchedulerEvent): number {
+  return props.cellWidth * daysBetweenOriginAndEventStart(event) + SPACING_OFFSET / 2
+}
 </script>
 
 <template>
-  <div class="vs-scheduler-event">
-  </div>
+  <div
+    class="vs-scheduler-timeline-event"
+    :style="{
+      width: eventWidth(event) + 'px',
+      height: `calc(100% - ${HEIGHT_OFFSET}px)`,
+      transform: `translate(${eventOffsetLeft(event)}px, ${HEIGHT_OFFSET / 2}px)`
+    }"
+  ></div>
 </template>
+
+<style>
+.vs-scheduler-timeline-event {
+  height: 100%;
+  background-color: lightcoral;
+  position: absolute;
+}
+</style>
--- src/components/subcomponents/VsSchedulerTimelineRow.vue
+++ src/components/subcomponents/VsSchedulerTimelineRow.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { ChronoUnit, LocalDate } from '@js-joda/core'
+import type { LocalDate } from '@js-joda/core'
 import { computed } from 'vue'
-import type { VsSchedulerEventGroup, VsSchedulerEvent } from '../VsScheduler.vue'
+import type { VsSchedulerEventGroup } from '../VsScheduler.vue'
+import VsSchedulerEventVue from './VsSchedulerEvent.vue'
 
 export interface VsSchedulerTimelineRowProps {
   group: VsSchedulerEventGroup
@@ -9,26 +10,7 @@ export interface VsSchedulerTimelineRowProps {
   earliestStartDate: LocalDate
 }
 
-const SPACING_OFFSET = 2
-const HEIGHT_OFFSET = 8
-
 const props = defineProps<VsSchedulerTimelineRowProps>()
-
-function numberOfDaysInEvent(event: VsSchedulerEvent): number {
-  return ChronoUnit.DAYS.between(event.startDate, event.endDate)
-}
-
-function eventWidth(event: VsSchedulerEvent): number {
-  return props.cellWidth * numberOfDaysInEvent(event) - SPACING_OFFSET - 1 // @TODO: Could use a CSS var to control spacing, -1 for border
-}
-
-function daysBetweenOriginAndEventStart(event: VsSchedulerEvent): number {
-  return ChronoUnit.DAYS.between(props.earliestStartDate, event.startDate)
-}
-
-function eventOffsetLeft(event: VsSchedulerEvent): number {
-  return props.cellWidth * daysBetweenOriginAndEventStart(event) + SPACING_OFFSET / 2
-}
 
 const backgroundStripePattern = computed(
   () => `repeating-linear-gradient(
@@ -43,23 +25,12 @@ const backgroundStripePattern = computed(
 
 <template>
   <div class="vs-scheduler-timeline-row" :style="{ background: backgroundStripePattern }">
-    <div
+    <vs-scheduler-event-vue
       v-for="(event, idx) in group.events"
-      class="vs-scheduler-timeline-item"
-      :style="{
-        width: eventWidth(event) + 'px',
-        height: `calc(100% - ${HEIGHT_OFFSET}px)`,
-        transform: `translate(${eventOffsetLeft(event)}px, ${HEIGHT_OFFSET / 2}px)`
-      }"
       :key="idx + event.startDate.toString() + event.endDate.toString()"
+      :cell-width="cellWidth"
+      :earliest-start-date="earliestStartDate"
+      :event="event"
     />
   </div>
 </template>
-
-<style>
-.vs-scheduler-timeline-item {
-  height: 100%;
-  background-color: lightcoral;
-  position: absolute;
-}
-</style>
