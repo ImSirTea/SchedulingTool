ae20c0f913d3fdee6b2866c326c6562715cee791
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-27 23:45:17 +0100

    Push through default props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import ItemScheduler from "@/components/scheduler/ItemScheduler.vue"
+import ItemScheduler from "@/components/scheduler/ItemScheduler.vue";
 </script>
 
 <template>
-  <div class="container">
-    <item-scheduler />
-  </div>
+	<div class="container">
+		<item-scheduler :rows="[]" :items="[]" />
+	</div>
 </template>
 
 <style scoped>
 .container {
-  width: 75vw;
-  height: 75vh;
+	width: 75vw;
+	height: 75vh;
 }
 </style>
